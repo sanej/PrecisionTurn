b86735ad86adbf47bb481c1adfe3acd54b425f49
sanej <88908918+sanej@users.noreply.github.com>
2024-09-03 12:21:33 -0700

    created config js file

---
+++ frontend/config.js
@@ -0,0 +1,9 @@
+// config.js
+window.config = {
+    baseUrl: 'http://localhost:8000' // Default to local server
+};
+
+// If running in a production environment, override the base URL
+if (window.location.hostname !== 'localhost') {
+    window.config.baseUrl = 'http://storage.googleapis.com/precisionturn-frontend/frontend';
+}
